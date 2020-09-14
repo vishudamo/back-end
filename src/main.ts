a46ad00a656c23f7c181e7364548cc8a28d753b8
D Vishwanathan <dvishwanathan@toastmasters.org>
2020-09-14 14:58:10 +0530

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Productivity Tool')
+    .setDescription('This project contains module for Coding, Billing and AR management')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
